79611b516283dac2a26b098ede2233e29f9cc12b
Chaim Shteinberg <a0533197133@gmail.com>
2025-07-30 16:34:11 +0300

    finish guards

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -1,21 +1,43 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Roles } from './roles.decorator';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector){}
+  constructor(
+    private reflector: Reflector,
+    private jwtService: JwtService
+  ) { }
+
   canActivate(
     context: ExecutionContext,
   ): boolean {
     const roles = this.reflector.get<string[]>('roles', context.getHandler())
-    if (!roles){
+    if (!roles) {
       return true
     }
-    const role = "commander"
+    const request = context.switchToHttp().getRequest()
+    const token = this.getToken(request)
+    if (!token) {
+      throw new Error("token not found")
+    }
+    try {
+      const payload = this.jwtService.verify(
+        token
+      )
+      request["user"] = payload
+    } catch (error) {
+      throw new Error(error)
+    }
+    const role = request.user.role 
     const hasRole = roles.includes(role)
     if (!hasRole)
       throw new Error("Access denied: insufficient permissions")
     return true;
+  }
+
+  private getToken(request: Request): string | undefined {
+    return request.headers?.authorization
   }
 }
--- src/shifts/shifts.module.ts
+++ src/shifts/shifts.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ShiftsController } from './shifts.controller';
 import { ShiftsService } from './shifts.service';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+        JwtModule.registerAsync({
+          imports: [ConfigModule], 
+          useFactory: async (configService: ConfigService) => ({
+            secret: configService.get<string>('JWT_SECRET'),
+          }),
+          inject: [ConfigService],
+    
+  })],
   controllers: [ShiftsController],
-  providers: [ShiftsService]
+  providers: [ShiftsService, RolesGuard]
 })
 export class ShiftsModule {}
