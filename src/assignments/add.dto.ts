7d18279b753cdcd79b52005b1864a341b1b5cacc
Chaim Shteinberg <a0533197133@gmail.com>
2025-07-31 13:03:50 +0300

    finish createAssignments

--- src/assignments/add.dto.ts
+++
@@ -1,3 +0,0 @@
-// export class addDto{
-//     assignment: string
-// }
--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -1,10 +1,7 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { AssignmentsService } from "./assignments.service";
 import { Assignments } from "./assignments.entity";
-
-// import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-// import { Assignment } from './IAssignments';
-// import { addDto } from './add.dto';
+import { createAssignmentsDto } from "./createAssignments.dto";
 
 @Controller('assignments')
 export class AssignmentsController {
@@ -23,8 +20,8 @@ export class AssignmentsController {
         return assignment;
     }
 
-    //     @Post()
-    //     addAssignments(@Body() assignment: addDto): void {
-    //         this.assignmentsService.addAssignments(assignment.assignment)
-    //     }
+    @Post()
+    async createAssignments(@Body() assignment: createAssignmentsDto): Promise<void> {
+        await this.assignmentsService.CreateAssignments(assignment.assignment)
+    }
 }
